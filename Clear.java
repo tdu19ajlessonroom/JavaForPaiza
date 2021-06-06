f7d5d98f431cd6e1952540b6caea9601569939ed
Alnya <69311410+Alnya@users.noreply.github.com>
2021-06-06 15:05:21 +0900

    Update Clear.java

--- Clear.java
+++ Clear.java
@@ -3,17 +3,20 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.nio.file.FileSystemException;
 
 public class Clear {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String source = "MainTemplate.txt";
         String target = "Main.java";
-        Path sourceFile = Paths.get(source);
-        Path targetFile = Paths.get(target);
         try {
+            Path sourceFile = Paths.get(source);
+            Path targetFile = Paths.get(target);
             Files.copy(sourceFile, targetFile, StandardCopyOption.REPLACE_EXISTING);
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (FileSystemException e) {
+            System.out.println("\n\n\t" + source + "\n\nもしくは\n\n\t" + target + "\n\nが開かれている可能性があります。");
+            System.out.println("\n\n上記のファイルを閉じてから再度実行してください。");
+            System.exit(1);
         }
     }
 }
